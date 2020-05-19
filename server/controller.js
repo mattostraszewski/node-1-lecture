164658f50b4381c962f6294fb1d720522a59a4d0
mattostraszewski <mrostraszewski@gmail.com>
2020-05-19 11:35:41 -0600

    Finished

--- server/controller.js
+++ server/controller.js
@@ -1,4 +1,5 @@
 const users = require('../data.json')
+let id = users[users.length - 1].id + 1
 
 module.exports = {
   getAllUsers: (req, res) => {
@@ -22,4 +23,52 @@ module.exports = {
       res.status(404).send('User not found')
     }
   },
+
+  createUser: (reg, res) => {
+    const { first_name, last_name, email } = req.body
+
+    const newUser = { id, first_name, last_name, email }
+
+    users.push(newUser)
+
+    id++
+  },
+
+  editUser: (req, res) => {
+    const { user_id } = req.params
+    const { first_name, last_name, email } = req.body
+    const index = users.findIndex(elem => elem.id === +user_id)
+    if (index === -1) {
+      return res.status(404).send('No user found')
+    } else {
+      // const updatedUser = {
+      //   id: +user_id,
+      //   first_name: first_name,
+      //   last_name: last_name,
+      //   email: email
+      // }
+      const updateUser = {
+        id: +user_id,
+        first_name: first_name || users[index].first_name,
+        last_name: last_name || users[index].last_name,
+        email: email || users[index].email
+
+      }
+      // users.splice(user_id, 1, updateUser)
+      users[index] = updateUser
+      res.status(200).send(users)
+    }
+  },
+
+  deleteUser: (req, res) => {
+    const { user_id } = req.params
+    const index = users.findIndex(elem => elem.id === +user_id)
+    if (index === -1) {
+      return res.status(404).send('No user exists')
+    } else {
+      users.splice(user_id, 1)
+      res.status(200).send('User has been deleted')
+    }
+
+  }
 } 
--- server/index.js
+++ server/index.js
@@ -3,7 +3,14 @@ const app = express()
 const ctrl = require('./controller')
 const SERVER_PORT = 4000
 
-app.use(express.json())
+app.use(express.json()) //parse's body. explain please
+
+//app.use is middleware
+
+app.use(function (req, res, next) {
+  console.log('We got a request')
+  next()
+}) //this will apply on every request to the server
 
 app.get('/api/matthew', (req, res) => {
   res.status(200).send('Matthew')
@@ -11,6 +18,12 @@ app.get('/api/matthew', (req, res) => {
 
 app.get('/api/users', ctrl.getAllUsers)
 app.get('/api/users/:user_id')
+
+app.post('/app/users', ctrl.createUser)
+
+app.put('/app/users/:user_id', ctrl.editUser)
+
+app.delete('/app.users/:user_id', ctrl.deleteUser)
 
 app.listen(SERVER_PORT, () =>
   console.log(`Server running on port ${SERVER_PORT}`))
